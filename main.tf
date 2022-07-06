1aafd64ec824d8d197e64c54df6184ca859ff605
Shalini Ramakrishnan <67656978+anihils@users.noreply.github.com>
2022-07-06 16:00:43 -0700

    Initial upload

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+# Cloud storage bucket
+resource "google_storage_bucket" "input-storage" {
+  name          = "terraform-bucket"
+  location      = var.gcp_region
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
+
+# VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+# SQL Database
+resource "google_sql_database" "sql-database" {
+  name     = "terraform-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "sql-database-instance" {
+  name             = "terraform-database-instance"
+  region           = var.gcp_region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+  deletion_protection  = "true"
+}
+
+# BigQuery Dataset
+resource "google_bigquery_dataset" "bq-dataset" {
+  dataset_id                  = "terraform_dataset"
+  description                 = "BigQuery Dataset generated using terraform script"
+  location                    = var.gcp_region
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "learn-resources-355417"
+}
+
+variable "gcp_region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-west4"
+}
+
+variable "gcp_zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "us-west4-b"
+}
