1628e31536e56aa2ea8a51d14fe8415093021d6d
Shalini Ramakrishnan <67656978+anihils@users.noreply.github.com>
2022-07-07 16:40:33 -0700

    Change storage bucket name to be unique

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 # Cloud storage bucket
-resource "google_storage_bucket" "terraform-bucket-0707" {
+resource "google_storage_bucket" "storage-bucket-anihils" {
   name          = "terraform-bucket"
   location      = var.gcp_region
   force_destroy = true
